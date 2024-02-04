9f8cc3d25a67753af81cac4c49faab96ede9c6e5
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-04 03:46:16 +0300

    feat:add TransactionFilter functionality that filters and sorts transactions

--- src/components/TransactionFilter.js
+++ src/components/TransactionFilter.js
@@ -1,7 +1,33 @@
 import React from "react";
+import "./TransactionFilter.css";
 
-function TransactionFilter() {
-  return <div>TransactionFilter</div>;
+function TransactionFilter({ search, onSearchChange }) {
+  return (
+    <div>
+      <div className="search-bar">
+        <label>
+          Search Transactions
+          <input
+            className="search-bar-input"
+            type="text"
+            name="search"
+            value={search}
+            placeholder="Type the description"
+            onChange={(e) => onSearchChange(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="sort-bar">
+        <label>
+          Sort Transactions By:
+          <select name="sort" id="sort">
+            <option value="default">Default</option>
+            <option value="alphabetical">Alphabetical</option>
+          </select>
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default TransactionFilter;
