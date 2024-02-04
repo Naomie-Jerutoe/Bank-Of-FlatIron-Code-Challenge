7a16644ed2bc470eef65887d0cab0f610cb937e4
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-04 03:34:23 +0300

    feat:renders TransactionList.js as a child component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import TransactionList from "./components/TransactionList";
 
 function App() {
   return (
     <div className="App">
       <h1>Bank Of Flatiron</h1>
+      <TransactionList />
     </div>
   );
 }
