65fd2a38b9c2af3e9037e3c5ea911dc4f9749802
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-04 03:38:02 +0300

    feat:add the TransactionItem functionality to render the transactions table

--- src/components/TransactionItem.js
+++ src/components/TransactionItem.js
@@ -1,7 +1,42 @@
 import React from "react";
+import "./TransactionItem.css";
 
-function TransactionItem() {
-  return <div>TransactionItem</div>;
+function TransactionItem({ transactions, handleDelete }) {
+  return (
+    <div className="table-container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+            <th>Delete Transaction</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.id}</td>
+              <td>{transaction.date}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.amount}</td>
+              <td>
+                <button
+                  className="delete-btn"
+                  onClick={() => handleDelete(transaction.id)}
+                >
+                  delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default TransactionItem;
